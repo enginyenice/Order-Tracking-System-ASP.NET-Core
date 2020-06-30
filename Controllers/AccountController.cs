578e3628bb7e795191b71701d2e0775a955fb8dc
Engin Yenice <enginyenice2626@gmail.com>
2020-07-01 02:28:26 +0300

    Logout Method

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -26,20 +26,22 @@ namespace SiparisTakip.Controllers
         #region LoginPost
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult LoginPost(string userMail,string userPassword)
+        public IActionResult LoginPost(string userMail, string userPassword)
         {
             int result = 0;
             result = _siparisTakipDB.Users.Where(u => u.userMail == userMail && u.userPassword == userPassword).Count();
-            if(result > 0)
+            if (result > 0)
             {
 
                 var userProfile = _siparisTakipDB.Users.Select(User => User).Where(u => u.userMail == userMail && u.userPassword == userPassword).FirstOrDefault();
 
+                int SessionUserId = userProfile.userId;
                 string SessionUserMail = userProfile.userMail;
                 string SessionUserName = userProfile.userName;
                 string SessionUserSurname = userProfile.userSurname;
                 string SessionUserPermission = userProfile.userPermission;
 
+                HttpContext.Session.SetInt32("userId", SessionUserId);
                 HttpContext.Session.SetString("userMail", SessionUserMail);
                 HttpContext.Session.SetString("userName", SessionUserName);
                 HttpContext.Session.SetString("userSurname", SessionUserSurname);
@@ -52,29 +54,30 @@ namespace SiparisTakip.Controllers
                 TempData["InfoType"] = "warning";
                 return RedirectToAction(nameof(AccountController.Index), "Account");
             }
-            
+
         }
         #endregion
         #region RegisterPost
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult RegisterPost(string userMail,string userName, string userSurname ,string userPassword, string password)
+        public IActionResult RegisterPost(string userMail, string userName, string userSurname, string userPassword, string password)
         {
             int result = 0;
             result = _siparisTakipDB.Users.Where(u => u.userMail == userMail && u.userPassword == userPassword).Count();
-            if(result == 0)
+            if (result == 0)
             {
                 User newUser = new User();
-                newUser.userMail         = userMail;
-                newUser.userName         = userName;
-                newUser.userSurname      = userSurname;
-                newUser.userPassword     = userPassword;
-                newUser.userPermission   = "Customer";
+                newUser.userMail = userMail;
+                newUser.userName = userName;
+                newUser.userSurname = userSurname;
+                newUser.userPassword = userPassword;
+                newUser.userPermission = "Customer";
                 _siparisTakipDB.Add(newUser);
                 _siparisTakipDB.SaveChanges();
                 TempData["Info"] = "Your account has been created";
                 TempData["InfoType"] = "success";
-            } else
+            }
+            else
             {
                 TempData["Info"] = "E-mail address is registered in the system.";
                 TempData["InfoType"] = "danger";
@@ -82,5 +85,15 @@ namespace SiparisTakip.Controllers
             return RedirectToAction(nameof(AccountController.Index), "Account");
         }
         #endregion
+        public IActionResult DeleteSession()
+        {
+            HttpContext.Session.Remove("userId");
+            HttpContext.Session.Remove("userMail");
+            HttpContext.Session.Remove("userName");
+            HttpContext.Session.Remove("userSurname");
+            HttpContext.Session.Remove("userPermission");
+
+            return RedirectToAction(nameof(AccountController.Index), "Account");
+        }
     }
 }
