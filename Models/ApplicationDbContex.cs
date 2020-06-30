21955a1f280698b25b34c754af9f61d8467407fa
Engin Yenice <enginyenice2626@gmail.com>
2020-06-30 16:34:54 +0300

    Sipariş Takip - ASP.NET FirstCommit

---
+++ Models/ApplicationDbContex.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiparisTakip.Models
+{
+    public class ApplicationDbContex : DbContext
+    {
+        public ApplicationDbContex(DbContextOptions<ApplicationDbContex> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiparisTakip.Models
+{
+    public class User
+    {
+        [Key]
+        public int userId { get; set; }
+        public string userName { get; set; }
+        public string userSurname { get; set; }
+        [Required]
+        public string userMail { get; set; }
+        public string userPassword { get; set; }
+    }
+}
