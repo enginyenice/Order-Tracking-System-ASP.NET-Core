51fb12314281d9c8fc6beb6eba1cb4f210177044
tolunayesergun <tolunay50@gmail.com>
2020-07-01 03:15:44 +0300

    ,,

--- wwwroot/build/js/custom.js
+++ wwwroot/build/js/custom.js
@@ -1785,7 +1785,7 @@ function init_SmartWizard() {
     $('#wizard_verticle').smartWizard({
         transitionEffect: 'slide'
     });
-
+   
     $('.buttonNext').addClass('btn btn-success');
     $('.buttonPrevious').addClass('btn btn-primary');
     $('.buttonFinish').addClass('btn btn-default');
