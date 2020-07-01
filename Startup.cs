46cf405ea180f36f469a08e2f939737684da34a3
tolunayesergun <tolunay50@gmail.com>
2020-07-01 22:57:49 +0300

    Json Circle

--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
 
 namespace SiparisTakip
 {
@@ -27,6 +23,12 @@ namespace SiparisTakip
             services.AddSession();
             services.AddDbContext<Models.SiparisTakipDB>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
+
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+            {
+                Formatting = Newtonsoft.Json.Formatting.Indented,
+                ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            };
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
