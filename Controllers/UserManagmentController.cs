d9738bf926f5e023fdd0b6e0219310ddfee14f3d
Engin Yenice <enginyenice2626@gmail.com>
2020-07-03 20:38:23 +0300

    Permission Edit Add.

--- Controllers/UserManagmentController.cs
+++ Controllers/UserManagmentController.cs
@@ -11,13 +11,67 @@ namespace SiparisTakip.Controllers
     {
         private readonly SiparisTakipDB _siparisTakipDB;
 
+
+
         public UserManagmentController(SiparisTakipDB context)
         {
             _siparisTakipDB = context;
         }
+
+
+        public bool SessionCont()
+        {
+            if (HttpContext != null)
+            {
+                if ((HttpContext.Session.GetString("userId")) != null &&
+                    HttpContext.Session.GetString("userPermission") == "Administrator")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         public IActionResult Index()
         {
+            if (SessionCont() == false)
+                return RedirectToAction("Index", "Account");
             return View(_siparisTakipDB.Users.ToList());
         }
+
+
+
+        public  IActionResult PermissionEdit(int? id)
+        {
+            if (SessionCont() == false)
+                return RedirectToAction("Index", "Account");
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = _siparisTakipDB.Users
+                .FirstOrDefault(m => m.userId == id);
+            if(user.userPermission == "Customer")
+            {
+                user.userPermission = "Administrator";
+            } else
+            {
+                user.userPermission = "Customer";
+            }
+             _siparisTakipDB.SaveChanges();
+
+            int SessionID = Int32.Parse(HttpContext.Session.GetString("userId"));
+
+            if(id == SessionID)
+            {
+                HttpContext.Session.SetString("userPermission", user.userPermission);
+            }
+
+
+
+            return RedirectToAction("Index", "UserManagment");
+        }
+
     }
 }
