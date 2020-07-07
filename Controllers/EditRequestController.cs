12832d56dd3362bdee16e77665dbf51fe5a73f90
Engin Yenice <enginyenice2626@gmail.com>
2020-07-07 18:41:17 +0300

    Image düzenle backend

--- Controllers/EditRequestController.cs
+++ Controllers/EditRequestController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SiparisTakip.Models;
+using SiparisTakip.Models.Tables;
+using System;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,11 +13,15 @@ namespace SiparisTakip.Controllers
 {
     public class EditRequestController : Controller
     {
+        [Obsolete]
+        private readonly IHostingEnvironment _evrimoment;
+
         private readonly SiparisTakipDB _siparisTakipDB;
 
-        public EditRequestController(SiparisTakipDB context)
+        public EditRequestController(SiparisTakipDB context,IHostingEnvironment evrimoment)
         {
             _siparisTakipDB = context;
+            _evrimoment = evrimoment;
         }
 
         public bool SessionCont()
@@ -38,15 +46,13 @@ namespace SiparisTakip.Controllers
             ViewBag.data = _siparisTakipDB.Departments.ToList();
             return View(_siparisTakipDB.Requests.Where(i=>i.requestId==id).Include(i=>i.user).FirstOrDefault());
         }
-
-  
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Edit([Bind("requestId,requestEstimatedPrice,requestDeliveryDate,requestSupplyCompany1," +
             "requestSupplyCompany2,requestSupplyCompany3," +
             "requestStatus,requestSpecies,requestQuantity," +
             "requestDescription,requestProductFeatures,requestExpensePlace," +
-            "requestProject,requestSteff,requestDepartment")] SiparisTakip.Models.Tables.Request req)
+            "requestProject,requestSteff,requestDepartment")] Request req)
         {
             bool session = SessionCont();
             if (session == false)
@@ -87,7 +93,43 @@ namespace SiparisTakip.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Obsolete]
+        public IActionResult EditImage(
+           [Bind("requestId","ImageFile")] Request request)
+        {
+            try
+            {
+                Random rastgele = new Random();
+                int sayi = rastgele.Next(5555, 25000);
+                string resimler = Path.Combine(_evrimoment.WebRootPath, "images");
+                string resimPath = "resimYok.jpg";
+                if (request.ImageFile != null)
+                {
+                    if (request.ImageFile.Length > 0)
+                    {
+                        using (var fileStream = new FileStream(Path.Combine(resimler, sayi + "-" + request.ImageFile.FileName), FileMode.Create))
+                        {
+                            request.ImageFile.CopyTo(fileStream);
+                        }
+                        resimPath = sayi + "-" + request.ImageFile.FileName;
+                    }
+                }
 
-     
+               
+                var thisRequest = _siparisTakipDB.Requests.Where(m => m.requestId == request.requestId).FirstOrDefault();
+                thisRequest.requestImage = resimPath;
+
+                _siparisTakipDB.Update(thisRequest);
+                _siparisTakipDB.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+            }
+            return RedirectToAction("Index", "PendingRequests");
+        }
+
+
     }
 }
