cccc432b3b801298d019aab281b297ced1d0cc5e
Engin Yenice <enginyenice2626@gmail.com>
2020-07-07 18:26:39 +0300

    Request Edit

--- Controllers/EditRequestController.cs
+++ Controllers/EditRequestController.cs
@@ -35,30 +35,47 @@ namespace SiparisTakip.Controllers
             {
                 return RedirectToAction("Index", "Home");
             }
-
+            ViewBag.data = _siparisTakipDB.Departments.ToList();
             return View(_siparisTakipDB.Requests.Where(i=>i.requestId==id).Include(i=>i.user).FirstOrDefault());
         }
 
   
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Index(int id, [Bind("requestId,requestImage,requestEstimatedPrice,requestDeliveryDate,requestSupplyCompany1,requestSupplyCompany2,requestSupplyCompany3,requestStatus,requestCreateAt,requestSpecies,requestQuantity,requestDescription,requestProductFeatures,requestExpensePlace,requestProject,requestSteff,requestDepartment,requestNo")] SiparisTakip.Models.Tables.Request req)
+        public IActionResult Edit([Bind("requestId,requestEstimatedPrice,requestDeliveryDate,requestSupplyCompany1," +
+            "requestSupplyCompany2,requestSupplyCompany3," +
+            "requestStatus,requestSpecies,requestQuantity," +
+            "requestDescription,requestProductFeatures,requestExpensePlace," +
+            "requestProject,requestSteff,requestDepartment")] SiparisTakip.Models.Tables.Request req)
         {
             bool session = SessionCont();
             if (session == false)
             {
                 return RedirectToAction("Index", "Home");
             }
-            if (id != req.requestId)
-            {
-                return NotFound();
-            }
 
             if (ModelState.IsValid)
             {
                 try
                 {
-                    _siparisTakipDB.Update(req);
+                    var thisRequest = _siparisTakipDB.Requests.Where(m => m.requestId == req.requestId).FirstOrDefault();
+
+                    thisRequest.requestDeliveryDate = req.requestDeliveryDate;
+                    thisRequest.requestDepartment = req.requestDepartment;
+                    thisRequest.requestDescription = req.requestDescription;
+                    thisRequest.requestEstimatedPrice = req.requestEstimatedPrice;
+                    thisRequest.requestExpensePlace = req.requestExpensePlace;
+                    thisRequest.requestProductFeatures = req.requestProductFeatures;
+                    thisRequest.requestProject = req.requestProject;
+                    thisRequest.requestQuantity = req.requestQuantity;
+                    thisRequest.requestSpecies = req.requestSpecies;
+                    thisRequest.requestStatus = req.requestStatus;
+                    thisRequest.requestSteff = req.requestSteff;
+                    thisRequest.requestSupplyCompany1 = req.requestSupplyCompany1;
+                    thisRequest.requestSupplyCompany2 = req.requestSupplyCompany2;
+                    thisRequest.requestSupplyCompany3 = req.requestSupplyCompany3;
+
+                    _siparisTakipDB.Update(thisRequest);
                     _siparisTakipDB.SaveChanges();
                 }
                 catch (DbUpdateConcurrencyException)
